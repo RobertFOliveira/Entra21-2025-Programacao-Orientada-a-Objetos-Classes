7563d0b354d4fe81ede7c9d80de1ff873e671863
ROBERT FERNANDES DE OLIVEIRA <robert_f_oliveira@estudante.sesisenai.org.br>
2025-06-30 17:12:48 -0300

    Primeiro commit

---
+++ src/projetoClassesObjetos3/Relatorios.java
@@ -0,0 +1,10 @@
+package projetoClassesObjetos3;
+
+public class Relatorios {
+
+	System.out.println("===RELATÓRIO===");
+	System.out.println("\nUnidade: " + unidadeDeBlumenau.nome);
+	System.out.println("Quantidade de Maças vendidas por ano: " + unidadeDeBlumenau.nMacasVendAno + " no valor de: R$ " + unidadeDeBlumenau.nMacasVendAno);
+	System.out.println("Quantidade de Laranjas vendidas por ano: " + unidadeDeBlumenau.nLaranjasVendAno + " no valor de: R$ " + unidadeDeBlumenau.valorLaranjas);
+
+}
